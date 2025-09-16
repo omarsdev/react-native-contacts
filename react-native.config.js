ce61a3ea450c92868220e4c2ccf58671939afabc
Omar Sukarieh <me@omars.dev>
2025-09-16 22:50:46 +0300

    fix(android): restore react-native.config.js for autolinking and include in package files

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dependency: {
+    platforms: {
+      android: {
+        sourceDir: './android',
+        packageImportPath: 'com.contactsupdated.ContactsLastUpdatedPackage',
+        packageInstance: 'new ContactsLastUpdatedPackage()',
+      },
+    },
+  },
+};
