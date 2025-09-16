01d227ea68b86bc0709b2373383c1197c417d4c4
Omar Sukarieh <me@omars.dev>
2025-09-16 22:11:30 +0300

    fix(ios): provide fallback promise typealiases

--- ios/NativeContactsLastUpdatedSpec.swift
+++ ios/NativeContactsLastUpdatedSpec.swift
@@ -1,5 +1,12 @@
 import Foundation
+#if canImport(React)
 import React
+#endif
+
+#if !canImport(React)
+public typealias RCTPromiseResolveBlock = (_ result: Any?) -> Void
+public typealias RCTPromiseRejectBlock = (_ code: String?, _ message: String?, _ error: Error?) -> Void
+#endif
 
 @objc(ContactsLastUpdatedSpec)
 protocol ContactsLastUpdatedSpec: RCTTurboModule {
